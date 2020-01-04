79c1b0f8706b97cd247578432ec42d695dbd6255
Pavel Shamshin <odan@selaz.org>
2020-01-04 11:48:01 +0300

    Update Location.php
    Location entity

--- src/Telegram/Entity/Location.php
+++ src/Telegram/Entity/Location.php
@@ -3,5 +3,26 @@
 namespace Selaz\Telegram\Entity;
 
 class Location {
-	
+
+    /** @var float */
+    private $longitude;
+    
+    /** @var float */
+    private $latitude;
+
+    public function setLatitude(?float $latitude): void {
+        $this->latitude = $latitude;
+    }
+
+    public function getLatitude(): ?float {
+        return $this->latitude;
+    }
+
+    public function setLongitude(?float $longitude): void {
+        $this->longitude = $longitude;
+    }
+
+    public function getLongitude(): ?float {
+        return $this->longitude;
+    }
 }
